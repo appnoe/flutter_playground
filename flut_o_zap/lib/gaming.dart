8c9681bedf5e1333e360f300ffa570e2e1894b35
Klaus M. Rodewig <kmr@appnoe.com>
2020-11-25 13:58:48 +0100

    👍🏻 - add navigation stack example
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

--- flut_o_zap/lib/gaming.dart
+++ flut_o_zap/lib/gaming.dart
@@ -3,8 +3,32 @@ import 'package:flutter/material.dart';
 class Gaming extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('Gaming')
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Show details'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Details"),
+      ),
+      body: Container(
+        color: Colors.greenAccent,
+      ),
     );
   }
 }
